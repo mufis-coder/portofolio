a3d3bf3678e91c3cb64759f7df98e86a9276f348
mufis-coder <muhamadfikrisunandar@gmail.com>
2024-01-27 16:10:39 +0700

    feat: animation in herosection

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -2,13 +2,21 @@
 import React from 'react'
 import Image from 'next/image'
 import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <section className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center place-items-center grid lg:place-items-start">
-            <h1 className="text-white max-w-2xl mb-4 lg:text-6xl text-4xl font-extrabold">
-                <span className="text-transparent text-6xl bg-clip-text bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+    <section className="lg:py-16">
+        <div className="grid grid-cols-1 sm:grid-cols-12">
+            <motion.div
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.5 }}
+                className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+                >
+                <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
                     Hello, I'm{" "}
                 </span>{" "}
                 <br></br>
@@ -32,26 +40,39 @@ const HeroSection = () => {
                 Backend Developer | ML/AI | Python | Java | Springboot
             </p>
             <div>
-                <button className="bg-gradient-to-br from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% hover:bg-slate-200 text-white px-6 py-3 rounded-full mr-4">
+                <Link
+                    href="/#contact"
+                    className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% hover:bg-slate-200 text-white"
+                    >
                     Hire Me
-                </button>
-                <button className="m-4 bg-gradient-to-br from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% px-1 py-1  text-white rounded-full">
+                </Link>
+                <Link
+                    href="https://drive.google.com/file/d/16Jrd1nS9NWrCMUrX4du4Zm13BwsWmetJ/view?usp=sharing"
+                    className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 hover:bg-slate-800 text-white mt-3"
+                    target="_blank"
+                    >
                     <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                         Download CV
                     </span>
-                </button>
+                </Link>
             </div>
-        </div>
-        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-            <div className="bg-[#35c8d3] lg:w-[400px] lg:h-[400px] w-[250px] h-[250px] rounded-full relative">
-                <Image
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.5 }}
+                className="col-span-4 place-self-center mt-4 lg:mt-0"
+                >
+                <div className="rounded-full bg-[#35c8d3] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+                    <Image
                     src="/images/profile.png"
-                    alt="profile image"
+                    alt="hero image"
                     className="w-[220px] h-[220px] lg:w-[360px] lg:h-[360px] absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                     width={1000}
                     height={1000}
-                />
-            </div>
+                    />
+                </div>
+            </motion.div>
         </div>
     </section>
   )
